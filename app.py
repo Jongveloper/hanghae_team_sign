b6cb39b2bbeebc5cca8bb12ab7bf3109316af8f5
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-06-10 10:59:06 +0900

    로그인시 메인페이지 최상단에 이름 나오게하기

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 
-
 app = Flask(__name__)
 from pymongo import MongoClient
 
-
-client = MongoClient('localhost',27017)
-db= client.sign
+client = MongoClient('localhost', 27017)
+db = client.sign
 SECRET_KEY = 'hanghae11team'
 
 import jwt
 import datetime
 import hashlib
-
-
-
-
-
 
 
 ############################################
@@ -34,22 +27,29 @@ def main():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+
 @app.route('/login')
 def login():
     id = "ID"
     password = "PassWord"
     return render_template('login.html', id=id, password=password)
 
+
 @app.route('/join')
 def join():
-    name= "이름"
+    name = "이름"
     id = "아이디"
     password = "비밀번호"
     re_password = "비밀번호 재확인"
     return render_template('join.html', name=name, id=id, password=password, re_password=re_password)
+
+
 @app.route('/main')
 def index():
-    return render_template('index.html')
+    user_info = db.user.find_one({"name"})
+    name = user_info
+    return render_template('index.html', name=name)
+
 
 ############################################
 ## 로그인 ####################################
@@ -69,18 +69,17 @@ def join_save():
             return jsonify({'result': 'fail', 'msg': '아이디가 중복되었습니다.'})
 
     if id_receive == "" or name_receive == "" or pw_receive == "":
-        return jsonify({'result':'fail', 'msg': "모두 입력해주세요!"})
+        return jsonify({'result': 'fail', 'msg': "모두 입력해주세요!"})
 
     if pw_receive != re_pw_receive:
-        return jsonify({'result':'fail', 'msg': '비밀번호가 일치하지 않습니다.'})
-
-
+        return jsonify({'result': 'fail', 'msg': '비밀번호가 일치하지 않습니다.'})
 
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
     db.user.insert_one({'name': name_receive, 'id': id_receive, 'pw': pw_hash})
 
     return jsonify({'result': 'success'})
+
 
 # 로그인 api
 @app.route('/api/login', methods=['POST'])
@@ -93,20 +92,20 @@ def api_login():
     result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
 
     if result is not None:
+
         payload = {
             'id': id_receive,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=1000)
         }
+
         # payload 암호화
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-    if id_receive == "" or pw_receive == "":
-        return jsonify({'result': 'fail', 'msg': "모두 입력해주세요!"})
 
         return jsonify({'result': 'success', 'token': token})
     else:
+        if id_receive == "" or pw_receive == "":
+            return jsonify({'result': 'fail', 'msg': "모두 입력해주세요!"})
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-
 
 
 @app.route('/api/name', methods=['GET'])
@@ -116,12 +115,12 @@ def api_valid():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         print(payload)
 
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id':0})
+        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
         return jsonify({'result': 'success', 'name': userinfo['name']})
     except jwt.ExpiredSignatureError:
         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
-        return jsonify({'result': 'fail', 'msg' : '로그인 정보가 존재하지 않습니다.'})
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 @app.route('/like_bu', methods=['POST'])
@@ -139,11 +138,12 @@ def like_b():
             # count = db.likes.count_documents({"post_id": tname})
 
         print(a)
-        return jsonify({"result": "success", 'msg': 'updated',  "post_id": a})
+        return jsonify({"result": "success", 'msg': 'updated', "post_id": a})
         # 좋아요 수 변경
 
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("login"))
+
 
 # 좋아요 api
 
@@ -163,8 +163,6 @@ def update_like():
             "league": league["league"]
         }
 
-
-
         if action_receive == "like":
             db.likes.insert_one(doc)
 
@@ -173,8 +171,7 @@ def update_like():
 
         count = db.likes.count_documents({"post_id": post_id_receive})
 
-
-        return jsonify({"result": "success", 'msg': 'updated',  "count": count})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
         # 좋아요 수 변경
 
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
@@ -193,14 +190,10 @@ def home():
 
 @app.route('/ranking', methods=["GET"])
 def get_ranking():
-
     ranking_list = list(db.teams.find({}, {'_id': False}))
 
-    return jsonify({'result': 'success', 'ranking_list': ranking_list })
-
+    return jsonify({'result': 'success', 'ranking_list': ranking_list})
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
